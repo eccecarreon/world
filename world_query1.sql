02be2766afa56f6c37010f134ee1f5736ed5e774
eccecarreon <153371886+eccecarreon@users.noreply.github.com>
2024-03-20 19:16:33 +0800

    commit answers
    with images

--- world_query1.sql
+++ world_query1.sql
@@ -7,37 +7,48 @@ FROM cities;
 SELECT *
 FROM countries;
 
-SELECT continent, COUNT(continent) AS 'number of countries'
-FROM countries
-GROUP BY continent
-ORDER BY COUNT(continent) DESC;
-
-SELECT countries.name, languages.language, languages.percentage 
-FROM languages
-JOIN countries ON countries.id = languages.id
-WHERE languages.language = "Greek"
-ORDER BY languages.percentage DESC;
-
 SELECT name, surface_area, population
 FROM countries
-WHERE surface_area<1000 OR population>1000000000;
+WHERE surface_area<501 AND population>100000;
 
-SELECT name, government_form, life_expectancy
+SELECT name, government_form, capital
 FROM countries
-WHERE government_form="Federal Republic" AND life_expectancy<75;
+WHERE government_form="Constitutional Monarchy"
+	AND life_expectancy > 75
+    AND capital > 200;
+
+SELECT region, COUNT(id) AS num_countries
+FROM countries
+GROUP BY region
+ORDER BY num_countries DESC;
+
+SELECT countries.name, languages.language, languages.percentage
+FROM countries
+JOIN languages ON countries.id = languages.country_id
+WHERE languages.language="Slovene"
+ORDER BY languages.percentage DESC;
+
+SELECT countries.name, COUNT(cities.id) as num_cities
+FROM countries
+JOIN cities ON cities.id = cities.country_id
+GROUP BY countries.id
+ORDER BY num_cities DESC;
+
+SELECT cities.name, cities.population
+FROM countries
+JOIN cities ON countries.id = cities.country_id
+WHERE countries.name="Mexico" AND cities.population>500000
+ORDER BY cities.population DESC;
+
+SELECT countries.name, languages.language, languages.percentage
+FROM countries
+JOIN languages ON countries.id = languages.country_id
+WHERE languages.percentage>89
+ORDER BY languages.percentage DESC;
 
 SELECT countries.name, cities.name, cities.district, cities.population
 FROM countries
-JOIN cities ON countries.id = cities.id
-WHERE countries.name="Mexico" AND cities.district="Veracruz" AND cities.population>100000;
-
-SELECT countries.name, cities.name, cities.population
-FROM countries
-JOIN cities ON countries.id = cities.id
-ORDER BY cities.population ASC
-LIMIT 10;
-
-SELECT countries.name, languages.language, languages.percentage 
-FROM languages
-JOIN countries ON countries.id = languages.id
-WHERE countries.name="Monaco" AND languages.percentage>10;
+JOIN cities ON countries.id = cities.country_id
+WHERE countries.name="Argentina"
+	AND cities.district="Buenos Aires"
+    AND cities.population>500000;
