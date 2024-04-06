9a8465d66fbdba83c02709cf9f34249d995667f3
eccecarreon <153371886+eccecarreon@users.noreply.github.com>
2024-04-06 20:26:39 +0800

    commits

--- world_query1.sql
+++ world_query1.sql
@@ -30,7 +30,7 @@ ORDER BY languages.percentage DESC;
 
 SELECT countries.name, COUNT(cities.id) as num_cities
 FROM countries
-JOIN cities ON cities.id = cities.country_id
+JOIN cities ON countries.id = cities.country_id
 GROUP BY countries.id
 ORDER BY num_cities DESC;
 
